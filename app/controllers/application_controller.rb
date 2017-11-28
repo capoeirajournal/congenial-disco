d0568768aa7ed592759f884c7546ceb683c46d69
Policerobots <capoeirajournal@gmail.com>
2017-11-28 15:44:54 -0500

    test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def bye
-    render html: "wtf ok bye."
+    render html: "no way...this is on Heroku?"
   end
 
 end
